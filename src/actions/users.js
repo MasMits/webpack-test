c339b77f4e73437345327de15becf9b07b6d1ade
MasMits <misha.mas22@gmail.com>
2023-06-30 21:23:36 +0300

    test: env API

--- src/actions/users.js
+++ src/actions/users.js
@@ -7,10 +7,13 @@ import {
 } from "../store/slices/users.slice";
 
 export const fetchUsers = (page = 1) => {
+    console.log(process.env.API);
     return async (dispatch) => {
         try {
             const response = await fetch(`${process.env.API}/users?page=${page}&count=6`);
             const data = await response.json();
+            console.log(data);
+
             dispatch(fetchUsersSuccess(data.users));
             if(page === 1){
                 dispatch(resetPages());

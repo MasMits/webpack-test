40673e050f2672ab09038dab935679c41bbbe61c
MasMits <misha.mas22@gmail.com>
2023-06-30 21:42:48 +0300

    test: different API name

--- src/actions/users.js
+++ src/actions/users.js
@@ -7,10 +7,10 @@ import {
 } from "../store/slices/users.slice";
 
 export const fetchUsers = (page = 1) => {
-    console.log(process.env.API);
+    console.log(process.env.API_KEY);
     return async (dispatch) => {
         try {
-            const response = await fetch(`${process.env.API}/users?page=${page}&count=6`);
+            const response = await fetch(`${process.env.API_KEY}/users?page=${page}&count=6`);
             const data = await response.json();
             console.log(data);
 

f95f2faa3270858385b93d7b73e8a7643014a344
MasMits <misha.mas22@gmail.com>
2023-06-30 13:37:54 +0300

    add: styles values

--- src/components/ui/Spinner/Spinner.jsx
+++ src/components/ui/Spinner/Spinner.jsx
@@ -3,11 +3,9 @@ import styles from './spinner.module.scss'
 
 export const Spinner = () => {
     return (
-        <svg className={styles.pl} viewBox="0 0 160 160" width="160" height="160">
+        <svg className={styles.pl} viewBox="0 0 160 160">
             <g className={styles.pl__ring_rotate}>
-                <circle className={styles.pl__ring_stroke} cx="80" cy="80" r="72" fill="none" stroke="#00BDD3"
-                        strokeWidth="16" strokeDasharray="452.39" strokeDashoffset="452" strokeLinecap="round"
-                        transform="rotate(-45 80 80)"/>
+                <circle className={styles.pl__ring_stroke} cx="80" cy="80" r="72"/>
             </g>
         </svg>
     );
